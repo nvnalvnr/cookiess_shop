afdd6f415c24ece8af561ffe148a491fbd5033e2
nvnalvnr <novanialefniar2211@gmail.com>
2026-06-18 15:39:47 +0700

    update database

--- config/database/cookiess_shop.sql
+++ config/database/cookiess_shop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 22, 2026 at 08:28 AM
+-- Generation Time: Jun 18, 2026 at 08:38 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.30
 
@@ -24,6 +24,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `quantity` int NOT NULL,
+  `total_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `status` enum('pending','processing','completed','cancelled') DEFAULT 'pending',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `product_id`, `quantity`, `total_price`, `status`, `created_at`) VALUES
+(1, 2, 5, 1, 0.00, 'completed', '2026-06-05 12:59:15'),
+(2, 2, 5, 1, 45000.00, 'completed', '2026-06-05 13:00:06'),
+(3, 2, 5, 1, 45000.00, 'processing', '2026-06-05 13:03:00'),
+(4, 3, 6, 2, 70000.00, 'pending', '2026-06-12 06:41:35');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -32,8 +58,18 @@ CREATE TABLE `products` (
   `name` varchar(100) NOT NULL,
   `price` decimal(10,2) NOT NULL,
   `stock` int NOT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `stock`, `created_at`, `image`) VALUES
+(4, 'Oatmeall dark Chocolate Cookie', 35000.00, 25, '2026-05-26 01:51:52', 'https://images.unsplash.com/photo-1558961363-fa8fdf82db35?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29va2llcyUyMGNoZXd5fGVufDB8fDB8fHww'),
+(5, 'green tea cake', 45000.00, 10, '2026-05-26 04:27:50', 'https://media.istockphoto.com/id/1408655409/photo/green-cake.webp?a=1&b=1&s=612x612&w=0&k=20&c=RLOdpXRHjfJUY5Nsk-PJLTIZDwSs8rzdspk51ij2ZZk='),
+(6, 'Soft Cookies Chewy', 35000.00, 8, '2026-06-05 10:19:09', 'https://images.unsplash.com/photo-1559622214-f8a9850965bb?q=80&w=765&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
 
 -- --------------------------------------------------------
 
@@ -55,11 +91,21 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`) VALUES
-(1, 'novani alexandrio', 'novanicantik@gmail.com', '$2y$10$/UWajElL.67I7xDBfJGcSew7aAPb006R0gX/4pFiFXtPw.yBvD.ZW', 'user', '2026-05-22 05:01:37');
+(1, 'novani alexandrio', 'novanicantik@gmail.com', '$2y$10$/UWajElL.67I7xDBfJGcSew7aAPb006R0gX/4pFiFXtPw.yBvD.ZW', 'admin', '2026-05-22 05:01:37'),
+(2, 'vani', 'vani@gmail.com', '$2y$10$bUyygt85BDq9sdACuRLny.AEvyAL4yXCUUy7g8zT1QmZuD6.kGivu', 'user', '2026-05-22 08:49:16'),
+(3, 'bunga', 'bunga@gmail.com', '$2y$10$biJBJ5mQHiQpaFqup3uTZe72aPyZqfensXR1hondX8inhlGMFkaCi', 'user', '2026-06-12 06:00:06');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `Product_id` (`product_id`);
 
 --
 -- Indexes for table `products`
@@ -79,16 +125,33 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
